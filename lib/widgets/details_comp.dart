799783493ff5e5ef33a7293e990546a630bd1b9c
Sanyam Sharma <sanyamsh20@gmail.com>
2023-02-16 20:03:22 +0530

    Implemented a basic widget for details

---
+++ lib/widgets/details_comp.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class DetailComponent extends StatelessWidget {
+  final double magnitude;
+  final String title;
+  final String sideText;
+
+  final TextStyle magStyle;
+  final TextStyle titleStyle;
+  final TextStyle sideTextStyle;
+
+  const DetailComponent({
+    super.key,
+    required this.magnitude,
+    required this.title,
+    this.sideText = "",
+    required this.magStyle,
+    required this.titleStyle,
+    this.sideTextStyle = const TextStyle(),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.alphabetic,
+          children: [
+            Text(
+              magnitude.toString(),
+              style: magStyle,
+            ),
+            (sideText != "")
+                ? Text(
+                    sideText.padLeft(3),
+                    style: sideTextStyle,
+                  )
+                : const SizedBox.shrink()
+          ],
+        ),
+        Text(
+          title,
+          style: titleStyle,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
