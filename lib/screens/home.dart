0a84fd4c2ad1e56ea975bbc496ef2928224321b2
Sanyam Sharma <sanyamsh20@gmail.com>
2023-02-19 05:59:31 +0530

    Minor Refactoring

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,8 @@ import 'package:airqualitymonitor/services/sensor_data.dart';
 import 'package:flutter/material.dart';
 import 'package:airqualitymonitor/widgets/air_quality_comp.dart';
 
+// import '../widgets/historical_data_comp.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -11,6 +13,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late Future<SensorData> sensorData;
+  bool showHistData = true;
+  void initState() {
+    super.initState();
+    sensorData = fetchSensorData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,23 +30,65 @@ class _HomeState extends State<Home> {
         backgroundColor: Theme.of(context).primaryColor,
       ),
       body: Center(
-        child: FutureBuilder(
-          future: fetchSensorData(),
-          builder: (BuildContext context, AsyncSnapshot<SensorData> snapshot) {
-            if (snapshot.hasData) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Padding(padding: EdgeInsets.only(top: 15)),
-                  AirQualityComponent(sensorData: snapshot.data!),
-                ],
-              );
-            } else if (snapshot.hasError) {
-              return const Text("Failed to Load Data");
-            }
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Padding(padding: EdgeInsets.only(top: 15)),
+            Stack(
+              alignment: Alignment.topCenter,
+              clipBehavior: Clip.none,
+              children: [
+                // if (showHistData)
+                //   Positioned(
+                //     top: 194,
+                //     child: FutureBuilder(
+                //       future: fetchHistoricalData(),
+                //       builder: ((context, snapshot) {
+                //         if (snapshot.connectionState == ConnectionState.done) {
+                //           if (snapshot.hasData) {
+                //             return HistoricalDataComponent(
+                //               histData: snapshot.data!,
+                //             );
+                //           } else if (snapshot.hasError) {
+                //             return const Text("Failed to Load Data");
+                //           }
+                //         }
+                //         return const CircularProgressIndicator();
+                //       }),
+                //     ),
+                //   ),
+                FutureBuilder(
+                  future: sensorData,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<SensorData> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        return AirQualityComponent(
+                          sensorData: snapshot.data!,
+                          reloadOnPressed: () {
+                            setState(() {
+                              sensorData = fetchSensorData();
+                            });
+                          },
+                          histDataOnPressed: () {
+                            setState(
+                              () {
+                                showHistData = !showHistData;
+                              },
+                            );
+                          },
+                        );
+                      } else if (snapshot.hasError) {
+                        return const Text("Failed to Load Data");
+                      }
+                    }
 
-            return const CircularProgressIndicator();
-          },
+                    return const CircularProgressIndicator();
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
