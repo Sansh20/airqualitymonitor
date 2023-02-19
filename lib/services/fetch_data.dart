1c07f60a7e267fad93fea0e79e6c289d991d145c
Sanyam Sharma <sanyamsh20@gmail.com>
2023-02-19 05:55:39 +0530

    Implemented fetching historical data

--- lib/services/fetch_data.dart
+++ lib/services/fetch_data.dart
@@ -12,3 +12,21 @@ Future<SensorData> fetchSensorData() async {
     throw Exception("Failed to Fetch Data");
   }
 }
+
+Future<Map<int, SensorData>> fetchHistoricalData() async {
+  final response =
+      await http.get(Uri.parse("http://127.0.0.1:3000/fetchHistData"));
+  DateTime today = DateTime.now();
+  var histDataMap = <int, SensorData>{};
+  if (response.statusCode == 200) {
+    var histData = jsonDecode(response.body);
+    for (String i in histData.keys) {
+      Map<String, dynamic> tmp = {i: histData[i]};
+      histDataMap[today.difference(DateTime.parse(i)).inDays] =
+          SensorData.fromJson(tmp);
+    }
+    return histDataMap;
+  } else {
+    throw Exception("Failed to Fetch Data");
+  }
+}
